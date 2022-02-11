88fbdd3a05b31011b853bf4df2a4a951e0e9c408
sergVe <testfrontol@gmail.com>
2022-02-12 01:45:06 +0300

    Решил задачу урока 1 модуля "Чат-боты"

--- main.py
+++ main.py
@@ -1,16 +1,57 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import os
+import time
+import requests
+from dotenv import load_dotenv
+import json
+from telegram.ext import ExtBot
 
 
-# Press the green button in the gutter to run the script.
+def get_code_review(devman_key, timestamp):
+    url = 'https://dvmn.org/api/long_polling/'
+    headers = {
+        'User-Agent': 'my-second-bot',
+        'Authorization': f'Token {devman_key}'
+    }
+    query_params = {
+        'timestamp': timestamp
+    }
+    response = requests.get(url, headers=headers, params=query_params)
+    response.raise_for_status()
+    return json.loads(response.text)
+
+
+def main():
+    load_dotenv()
+    timestamp = None
+    bot = ExtBot(token=os.getenv('TELEGRAM_KEY'))
+
+    while True:
+
+        try:
+            server_answer = get_code_review(devman_key=os.getenv('DEVMAN_KEY'), timestamp=timestamp)
+            print(server_answer)
+            timestamp = server_answer.get('timestamp_to_request', None)
+
+            if not timestamp:
+                last_review = server_answer['new_attempts'][-1]
+                lesson_title = last_review['lesson_title']
+                review_answer = 'К сожалению, в работе нашлись ошибки' if last_review['is_negative'] \
+                    else 'Преподавателю всё понравилось, можете приступать к следующему уроку'
+                lesson_url = last_review['lesson_url']
+                bot.send_message(chat_id=os.getenv('CHAT_ID'), text=f'У Вас проверили работу "{lesson_title}"\n'
+                                                                    f'{review_answer}\n'
+                                                                    f'Ссылка на урок: {lesson_url}')
+
+        except requests.exceptions.HTTPError as e:
+            print(e)
+            break
+        except requests.exceptions.ReadTimeout as e:
+            print(e)
+            time.sleep(180)
+        except requests.exceptions.ConnectionError as e:
+            print(e)
+            time.sleep(600)
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    main()
