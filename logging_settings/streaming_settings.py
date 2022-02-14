6376388fe354288f3019518b0d887f78e2163a51
sergVe <testfrontol@gmail.com>
2022-02-14 21:06:06 +0300

    Изменил настройки логгера

--- logging_settings/streaming_settings.py
+++ logging_settings/streaming_settings.py
@@ -1,10 +1,13 @@
 import logging
 
-logging.basicConfig(encoding='utf-8', level=logging.DEBUG)
+logging.basicConfig(encoding='utf-8', level=logging.WARNING)
 streaming_logger = logging.getLogger("streaming logger")
-streaming_logger.setLevel(logging.DEBUG)
+streaming_logger.setLevel(logging.WARNING)
 logger_handler = logging.StreamHandler()
-logger_handler.setLevel(logging.DEBUG)
+logger_handler.setLevel(logging.WARNING)
 formatter = logging.Formatter('%(asctime)s %(module)s %(processName)s  %(levelname)s: %(message)s')
 logger_handler.setFormatter(formatter)
 streaming_logger.addHandler(logger_handler)
+
+if __name__ == '__main__':
+    pass
